ee6f351ebb1f7d4288ae6ebb894e81881f6ad1f6
balajiprabhu-github <balajiprabhu008@gmail.com>
2020-04-05 17:13:44 +0530

    Account Management using Encapsulation

---
+++ src/com/balajiprabhu/Account.java
@@ -0,0 +1,25 @@
+package com.balajiprabhu;
+
+public class Account {
+
+    private float balance;
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        if (balance > 0)
+            this.balance = balance;
+    }
+
+    public void deposit(float amount) {
+        if (amount > 0)
+            balance += amount;
+    }
+
+    public void withDrawl(float amount) {
+        if (amount > 0)
+            balance -= amount;
+    }
+}
--- src/com/balajiprabhu/Main.java
+++ src/com/balajiprabhu/Main.java
@@ -3,11 +3,11 @@ package com.balajiprabhu;
 public class Main {
 
     public static void main(String[] args) {
-        TaxCalculator taxCalculator = calculateTax();
-        System.out.println(taxCalculator.calculateTax());
+        Account account = new Account();
+        account.setBalance(20000);
+        account.deposit(18000);
+        account.withDrawl(23000);
+        System.out.println(account.getBalance());
     }
 
-    private static TaxCalculator calculateTax() {
-        return new TaxCalculatorQuarterOne();
-    }
 }
